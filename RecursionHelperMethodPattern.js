b113ffa05ddd8500bc172dbd8563188244587317
nihiru <nikhil.m.alors@gmail.com>
2020-09-16 09:25:59 +0530

    example of Recursive method pattern

--- RecursionHelperMethodPattern.js
+++ RecursionHelperMethodPattern.js
@@ -1,3 +1,4 @@
+// Pattern for helper method recursion
 function outer(input){
     var outerScopedVariable = []
     function helper(helperInput){
@@ -7,3 +8,29 @@ function outer(input){
     helper(input)
     return outerScopedVariable
 }
+
+/**
+ * 
+ * @param {Array} arr  - from which odd or even are found
+ */
+function collectOddsValue(arr){
+    let result = [];
+
+    function helper(helperInput){
+        if(helperInput.length === 0){
+            return;
+        }
+
+        if(helperInput[0] %2 !== 0){
+            // push only items that are odd
+            result.push(helperInput[0])
+        }
+        // slicing array from the current position to the end of array 
+        helper(helperInput.slice(1))
+    }
+    // calling inner recursive function
+    helper(arr)
+    return result;
+}
+
+console.log(collectOddsValue([1,2,3,4,5,6,7,8]))
