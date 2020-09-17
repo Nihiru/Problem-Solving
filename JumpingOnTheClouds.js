da25190b67514169bfa8558f992b0401c4744d5d
nihiru <nikhil.m.alors@gmail.com>
2020-09-17 17:26:40 +0530

    Code refactored

--- JumpingOnTheClouds.js
+++ JumpingOnTheClouds.js
@@ -25,3 +25,24 @@ function jumpingOnClouds(arr){
 console.log(jumpingOnClouds([0,1,0,0,0,1,0,0,1,0,0]))
 console.log(jumpingOnClouds([0,0,1,0,0,1,0]))
 console.log(jumpingOnClouds([0,0,0,0,1,0]))
+
+function jumpingOnCloudsRefactor(arr){
+    let start = 0;
+    let end = arr.length - 1;
+    let count = 0
+    while(start < end){
+        if(arr[start+2] === 0){
+            count++;
+            start+=2;
+        }
+        else{
+            count++;
+            start+=1;
+        }
+    }
+    return count
+}
+
+console.log(jumpingOnCloudsRefactor([0,1,0,0,0,1,0,0,1,0,0]))
+console.log(jumpingOnCloudsRefactor([0,0,1,0,0,1,0]))
+console.log(jumpingOnCloudsRefactor([0,0,0,0,1,0]))
