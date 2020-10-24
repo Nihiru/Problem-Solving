a7b1f60829e7761556a31a43fa06cf229045ee1d
nihiru <nikhil.m.alors@gmail.com>
2020-10-24 18:26:48 +0530

    Implementing HashTable

---
+++ HashTable.js
@@ -0,0 +1,54 @@
+class HashTable {
+    constructor(size = 17) {
+        this.keyMap = new Array(size)
+    }
+
+    _hash(key) {
+        let total = 0;
+        let WEIRD_PRIME = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96;
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+        }
+        return total;
+    }
+
+    set(key, value) {
+        let index = this._hash(key)
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = []
+        }
+        this.keyMap[index].push([key, value])
+
+    }
+
+    get(key) {
+        let index = this._hash(key)
+        let found = 0
+        if (this.keyMap[index]) {
+            for (let i=0 ; i<this.keyMap[index].length;i++) {
+                if (this.keyMap[index][i][0] === key) {
+                    return this.keyMap[index][i]
+                }
+            }
+        }
+        return undefined
+    }
+
+    print() {
+        console.log(this.keyMap);
+    }
+}
+
+
+let ht = new HashTable();
+ht.set('maroon', '#800000')
+ht.set('yellow', '#FFFF00')
+ht.set('olive', '#808000')
+ht.set('salmon', '#FA8072')
+ht.set('mediumvioletred', '#C71585')
+console.log(ht.get('yellow'))
+console.log(ht.get('polive'))
+console.log(ht.get('olive'))
+ht.print()
