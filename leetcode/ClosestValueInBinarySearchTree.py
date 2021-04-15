c924d09a93ec37bbe897a08f7044112c589a7c99
Nikhil M <nikhil.m.alors@gmail.com>
2021-04-15 20:59:22 +0530

    Adding closest values in binary search tree iteratively

--- leetcode/ClosestValueInBinarySearchTree.py
+++ leetcode/ClosestValueInBinarySearchTree.py
@@ -9,18 +9,49 @@ Complexity:
 """
 
 
-def findClosestValueInBST(tree, target):
-    return findClosestValueInBSTHelper(tree, target, float("inf"))
+def findClosestValueInBST_R(tree, target):
+    return findClosestValueInBSTHelper_R(tree, target, float("inf"))
 
 
-def findClosestValueInBSTHelper(tree, target, closest):
+def findClosestValueInBSTHelper_R(tree, target, closest):
     if tree is None:
         return closest
     if abs(target - closest) > abs(target - tree.value):
         closest = tree.value
     if target < tree.value:
-        return findClosestValueInBSTHelper(tree.left, target, closest)
+        return findClosestValueInBSTHelper_R(tree.left, target, closest)
     elif target > tree.value:
-        return findClosestValueInBSTHelper(tree.right, target, closest)
+        return findClosestValueInBSTHelper_R(tree.right, target, closest)
     else:
         return closest
+
+# iterative implementation
+
+
+"""
+Complexity:
+    Time:
+        Average: O(Log(N))
+        Worst: O(N)
+    Space:
+        Average: O(1)
+        Worst: O(1)
+"""
+
+
+def findClosestValueInBST_I(tree, target):
+    return findClosestValueInBSTHelper_I(tree, target, float("inf"))
+
+
+def findClosestValueInBSTHelper_I(tree, target, closest):
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        elif target > currentNode.value:
+            currentNode = currentNode.right
+        else:
+            break
+    return closest
