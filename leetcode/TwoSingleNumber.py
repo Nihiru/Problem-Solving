18c177e49410ad99740728652fb34033364982e3
Nikhil M <nikhil.m.alors@gmail.com>
2021-05-17 18:46:57 +0530

    counting the least occurence of a number when compared to static occurence of numbers

--- leetcode/TwoSingleNumber.py
+++ leetcode/TwoSingleNumber.py
@@ -21,3 +21,27 @@ def single_number_II_one_line_solution(nums):
 
 print(single_number_II_one_line_solution(
     [2, 2, 2, 3, 3, 3, 4, 4, 4, 1, 5, 5, 5]))
+
+
+def single_number_II_bit_manipulation(nums):
+    ans = 0
+    x = 1
+    """bitwise left shift operator moves the bits of its first operand to the left by the number of places 
+    specified in the second operand    
+    """
+    mask = (x << 31)
+    while(mask):
+        count = 0
+        for i in list(range(len(nums))):
+            # here (mask & nums[i]) returns 0 or 1 based on least-significant bit of mask
+            if mask & nums[i]:
+                count += 1
+        if count % 3 != 0:
+            ans += mask
+        mask >>= 1
+    return ans
+
+
+print(single_number_II_bit_manipulation(
+    [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 5]
+))
