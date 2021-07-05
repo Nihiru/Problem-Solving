49184408b87022b98eea2f2ec78f1bb12144551c
Nihiru <nikhil.m.alors@gmail.com>
2021-07-05 19:39:49 +0530

    Finding duplicate by dividing array into 0...k and k...n-1

--- leetcode/FindDuplicate.py
+++ leetcode/FindDuplicate.py
@@ -49,29 +49,64 @@ return true if there are two distinct indices i and j in the array such that num
 3)  and the elements need to be same based on the description
 
 """
-def find_duplicate_II(elements, k, approach=0):
+def find_duplicate_II(elements, k=None, approach=1):
     len_ele = len(elements)
-    index_dict =  {} # how much space 
-    arr = [] # how much space does this array occupies ?
+    if not k:
+        k = len_ele
     """
     Complexity
 
     1) Time: O(N) as each element is traversed and O(1) for accessing elements
     2) Space: O(N) due to use of hash tables
     """
-    for i in range(len_ele):
-        if elements[i] in index_dict:
-            index_dict[elements[i]] = (i - index_dict[elements[i]])
-            arr.append(index_dict[elements[i]])
-        else:
-            index_dict[elements[i]] = i
+    if approach == 1:
+        index_dict =  {} # how much space 
+        arr = [] # how much space does this array occupies ?
+        for i in range(len_ele):
+            if elements[i] in index_dict:
+                index_dict[elements[i]] = (i - index_dict[elements[i]])
+                arr.append(index_dict[elements[i]])
+            else:
+                index_dict[elements[i]] = i
 
-    if len(arr):
-        for ele in arr:
-            if ele <= k:
+        if len(arr):
+            for ele in arr:
+                if ele <= k:
+                    return True
+    
+    elif approach == 2:
+        """
+        1)  Changing the  
+        """
+        if k <= 0:
+            return False
+        # if the range exceeds the given length of array then restrict to `k` or take the length of the array as `k`
+        k = min(k, len_ele)
+
+        uniq_elements = dict()
+        """
+        1)  Dividing the search in 2 methods 
+            1)  first search from i to k 
+            2)  second search from k to n 
+        2)  Complexity
+            1)  Space: O(K) as set holds only the elements based on `k` position
+            2)  Time: O(K) as the traversal happens only within the specified `k` position
+        """
+        for i in range(k):
+            if elements[i] in uniq_elements:
                 return True
+            uniq_elements[elements[i]] = i
+
+        for i in range(k, len_ele):
+            if elements[i] in uniq_elements:
+                return True
+            uniq_elements[elements[i]] = i
+    else:
+        pass
 
     return False
 
 
-print(find_duplicate_II([1,2,1,2], 2))
+print(find_duplicate_II([1,2,3,1], 3, 2))
+print(find_duplicate_II([1,0,1,1],3,2))
+# print(find_duplicate_II([1,2,3,1,2,3], 2, 2)) # failing 
