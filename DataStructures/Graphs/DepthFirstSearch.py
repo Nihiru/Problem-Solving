093e7dbf1665cb10700afd5f7a9e63f7ef377c00
Nihiru <nikhil.m.alors@gmail.com>
2021-09-01 19:58:57 +0530

    DepthFirstSearch incomplete

--- DataStructures/Graphs/DepthFirstSearch.py
+++ DataStructures/Graphs/DepthFirstSearch.py
@@ -6,18 +6,36 @@ class DepthFirstSearch:
     def add_node(self, u, v, w=0):
         if u not in self.graph:
             self.graph[u] = []
-        else:
-            self.graph[u].append({
-                v: w
-            })
+        self.graph[u].append({
+            v: w
+        })
 
-    def calculate_nodes_in_graph(self):
-        pass
+    def traverse(self, node=None):
+        visited = []
+        if len(self.graph):
+            for key, values in self.graph.items():
+                if len(values):
+                    for next_node in values:
+                        if next_node not in visited:
+                            pass
+        else:
+            return "Empty graph"
 
     def view_graph(self):
-        for keys, values in self.graph.items():
-            pass
+        if len(self.graph):
+            for key, values in self.graph.items():
+                print(f"Node: {key} connected nodes: {values}")
+        else:
+            print("Empty dictionary")
 
 
 dfs_obj = DepthFirstSearch()
-dfs_obj.add_node()
+dfs_obj.view_graph()
+dfs_obj.add_node(1, 2, 1)
+dfs_obj.add_node(1, 3, 2)
+dfs_obj.add_node(2, 4, 2)
+dfs_obj.add_node(3, 5, 3)
+dfs_obj.add_node(4, 5, 2)
+dfs_obj.add_node(5, 6, 5)
+
+dfs_obj.view_graph()
