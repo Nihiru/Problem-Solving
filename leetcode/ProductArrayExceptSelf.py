d3e3d3bd1f38a126f61344d9ab771ea10e222a86
Nihiru <nikhil.m.alors@gmail.com>
2021-09-30 19:00:44 +0530

    Product array except self without division - Solution 1

--- leetcode/ProductArrayExceptSelf.py
+++ leetcode/ProductArrayExceptSelf.py
@@ -2,6 +2,9 @@
 -) Problem Description:
     -) From the given array calculate the product of all numbers except the current one
     -) Write an algorithm that conforms to O(n) time without using division operator 
+    -) Solving the problem in O(1)
+-) Outcome
+    -) Get the multiplied value of each number in the list except the curreny selected one
 """
 
 
@@ -19,6 +22,42 @@ def product_array_except_self(arr):
 
 
 # print(product_array_except_self([1, 2, 3, 4])) # [24, 12, 8, 6]
+def product_array_except_rule_1(nums):
+    """
+    1) Multiplying the left hand side and right side of the selected number 
+    2) Working:
+        1) Number present in the LHS is multiplied with the next number in the list and cached
+        2) The multiplied number is cached and used as reference to multiplied to the next number in the list
+        3) Same goes for RHS where the cached value is dependent of the previous number in the list
+        3) This is stopped until the position of the selected number is reached
+        4) Multiply both the LHS and RHS to get the expected outcome
+    """
+    output = [1] * len(nums)
+    left_hand_arr = [1] * len(nums)
+    right_hand_arr = [1] * len(nums)
 
-def product_array_except_self_optimized(arr):
-    pass
+    for i in range(1, len(nums)):
+        left_hand_arr[i] = left_hand_arr[i-1] * nums[i-1]
+
+    for i in range(len(nums) - 2, -1, -1):
+        right_hand_arr[i] = right_hand_arr[i+1] * nums[i+1]
+
+    for i in range(len(nums)):
+        output[i] = left_hand_arr[i] * right_hand_arr[i]
+
+    return output
+
+
+def product_array_except_rule_2(nums):
+    output = [1] * len(nums)
+    L = len(output)
+    for i in range(1, L):
+        output[i] = output[i-1] * nums[i-1]
+    r = 1
+    for i in range(L-1, -1, -1):
+        output[i] = output[i] * r
+        r = r * nums[i]
+    return output
+
+
+print(product_array_except_rule_1([1, 2, 3, 4]))
