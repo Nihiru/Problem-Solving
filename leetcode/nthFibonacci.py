b0a2284b4eee1b2574c7a7246227b69a49e17e65
Nihiru <nikhil.m.alors@gmail.com>
2021-10-20 18:12:16 +0530

    Minimum cost climbing stairs

--- leetcode/nthFibonacci.py
+++ leetcode/nthFibonacci.py
@@ -111,4 +111,16 @@ def climbing_stairs(n, mem={1: 1, 2: 1}):
     return mem[n]
 
 
-print(climbing_stairs(4))
+# print(climbing_stairs(4))
+
+
+def min_cost_climbing_stairs(cost):
+    cost.append(0)
+    for i in range(len(cost) - 3, -1, -1):
+        cost[i] += min(cost[i+1], cost[i+2])
+
+    return min(cost[0], cost[1])
+
+
+cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+print(min_cost_climbing_stairs(cost))
