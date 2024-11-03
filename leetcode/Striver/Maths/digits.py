335661d1efb741a27f452f9b770fe4b9617ca2a2
Nihiru <nikhil.m.developer@gmail.com>
2024-11-03 17:24:44 +0530

    feat: reverse of a number

--- leetcode/Striver/Maths/digits.py
+++ leetcode/Striver/Maths/digits.py
@@ -25,10 +25,27 @@ Uses of logarithms in CS:
     -) log1p(a)
         -) Specifically designed to provide accurate results for values of `a` that are close to 0.
         -) It avoids numerical errors when using math.log(1+a) directly for small a.
-    
+        -) Its often used in numerical computations where precision is important, especially in statistical and machine learning algorithms
+
+Time Complexity
+    -) Since there is an existence of divison we're going with O(log10(number)). Why log10? because we're dividing by 10
 """
 import math
 def count_using_log(number):
     return int(math.log(number, 10)) + 1
 
-print(count_using_log(7789))
+# print(count_using_log(7789))
+
+""" 
+Statement 3: Reverse a number
+Description:  Maths tricks can help solve the problem
+"""
+def reverse_number(number):
+    rev_num = 0
+    while number > 0:
+        last_digit = int(number % 10)
+        rev_num = (rev_num * 10) + last_digit
+        number = number // 10
+    return rev_num
+
+print(reverse_number(7789))
